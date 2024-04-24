73792c45f9f9c3319d996c32deacf8d13b7e5c15
Heder Alves <alvesheder92@gmail.com>
2024-04-24 18:40:19 -0300

    feat: add the interfaces for the components

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,12 +1,44 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface Collaborator {
+    node: {
+        name: string;
+    };
+}
+
+export interface BranchRef {
+    target: {
+        history: {
+            totalCount: number;
+        };
+    };
+}
+
+export interface IssueConnection {
+    totalCount: number;
+}
+
+export interface PullRequestConnection {
+    totalCount: number;
+}
 export interface Repository {
     name: string;
     description: string;
     updatedAt: string;
-    collaborators: any;
-    pullRequests: any;
-    issues: any;
-    defaultBranchRef: any;
+    collaborators: {
+        edges: Collaborator[];
+    };
+    pullRequests: PullRequestConnection;
+    issues: IssueConnection;
+    defaultBranchRef: BranchRef;
+}
+
+export interface RepositoryListProps {
+    repositories: Repository[];
+    onRepositoryClick: (repository: Repository) => void;
+}
+
+export interface HeaderProps {
+    searchTerm: string;
+    onSearchChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export interface RootState {
@@ -31,4 +63,17 @@ export interface Repository {
 export interface ModalState {
     isOpen: boolean;
     selectedRepository: Repository | null;
+}
+
+export interface ModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    title: string;
+    description: string;
+    firstText: string;
+    secondText: string;
+    thirdText: string;
+    firstData: number;
+    secondData: number;
+    thirdData: number;
 }
