c93d4bbf4ce67e0a1df87fbe59ae3ada4f1a0081
Heder Alves <alvesheder92@gmail.com>
2024-04-24 00:39:07 -0300

    feat: create the modal with redux

---
+++ src/redux/reducers/modalReducer.ts
@@ -0,0 +1,27 @@
+import { Action, ModalState } from "../../interfaces";
+
+export const initialState: ModalState = {
+    isOpen: false,
+    selectedRepository: null,
+};
+
+const modalReducer = (state = initialState, action: Action) => {
+    switch (action.type) {
+        case 'OPEN_MODAL':
+            return {
+                ...state,
+                isOpen: true,
+                selectedRepository: action.payload,
+            };
+        case 'CLOSE_MODAL':
+            return {
+                ...state,
+                isOpen: false,
+                selectedRepository: null,
+            };
+        default:
+            return state;
+    }
+};
+
+export default modalReducer;
